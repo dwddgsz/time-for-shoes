1c34f24e53109b78c853da2b09c93e31272a386d
dwddgsz <dwddgsz@gmail.com>
2020-10-31 19:37:13 +0100

    add: products data

---
+++ src/assets/products-shoes.js
@@ -0,0 +1,61 @@
+import shoesFirst from './images/shoes-images/product-1.jpg';
+import shoesSecond from './images/shoes-images/product-2.jpg';
+import shoesThird from './images/shoes-images/product-3.jpg';
+import shoesFourth from './images/shoes-images/product-4.jpg';
+import shoesFifth from './images/shoes-images/product-5.jpg';
+import shoesSixth from './images/shoes-images/product-6.jpg';
+
+
+
+
+export const productsShoes = [
+    {
+        id:1,
+        name: 'a pair of shoes',
+        img: shoesFirst,
+        value: 99,
+        inCart: false,
+        amount: 0,
+    },
+    {
+        id:2,
+        name:'a pair of shoes',
+        img: shoesSecond,
+        value: 119,
+        inCart: false,
+        amount: 0,
+    },
+    {
+        id:3,
+        name:'a pair of shoes',
+        img: shoesThird,
+        value: 79,
+        inCart: false,
+        amount: 0,
+    },
+    {
+        id:4,
+        name:'a pair of shoes',
+        img: shoesFourth,
+        value: 99,
+        inCart: false,
+        amount: 0,
+    },
+    {
+        id:5,
+        name:'a pair of shoes',
+        img: shoesFifth,
+        value: 129,
+        inCart: false,
+        amount: 0,
+    },
+    {
+        id:6,
+        name:'a pair of shoes',
+        img: shoesSixth,
+        value: 59,
+        inCart: false,
+        amount: 0,
+    },
+
+]
---
+++ src/assets/products-watches.js
@@ -0,0 +1,59 @@
+import watchFirst from './images/watches-images/product-1.jpg';
+import watchSecond from './images/watches-images/product-2.jpg';
+import watchThird from './images/watches-images/product-3.jpg';
+import watchFourth from './images/watches-images/product-4.jpg';
+import watchFifth from './images/watches-images/product-5.jpg';
+import watchSixth from './images/watches-images/product-6.jpg';
+
+
+export const productsShoes = [
+    {
+        id:7,
+        name: 'watch',
+        img: watchFirst,
+        value: 499,
+        inCart: false,
+        amount: 0,
+    },
+    {
+        id:8,
+        name:'watch',
+        img: watchSecond,
+        value: 519,
+        inCart: false,
+        amount: 0,
+    },
+    {
+        id:9,
+        name:'watch',
+        img: watchThird,
+        value: 379,
+        inCart: false,
+        amount: 0,
+    },
+    {
+        id:10,
+        name:'watch',
+        img: watchFourth,
+        value: 599,
+        inCart: false,
+        amount: 0,
+    },
+    {
+        id:11,
+        name:'watch',
+        img: watchFifth,
+        value: 629,
+        inCart: false,
+        amount: 0,
+    },
+    {
+        id:12,
+        name:'watch',
+        img: watchSixth,
+        value: 59,
+        inCart: false,
+        amount: 0,
+    },
+
+]
