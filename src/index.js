dfc3952cadb04a919b56bb5b2020a9359685e1b4
dwddgsz <dwddgsz@gmail.com>
2020-11-01 16:08:07 +0100

    add:local storage

--- src/index.js
+++ src/index.js
@@ -6,9 +6,38 @@ import rootReducer from './reducers/reducers';
 import App from './App';
 
 
+const loadState = () => {
+    try{
+        const serializedState = localStorage.getItem('persistedState');
+        if (serializedState === null) {
+            return undefined
+        } else {
+            return JSON.parse(serializedState);
+        }
+    }
+    catch(error) {
+        return undefined
+    }
+}
+
+const saveState = (state) => {
+try {
+    localStorage.setItem('persistedState',JSON.stringify(state));
+}
+catch(error) {
+    
+}
+}
+
+const persistedState = loadState();
 const store = createStore(
     rootReducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+store.subscribe(()=>{
+    saveState(store.getState());
+})
 
 
 
