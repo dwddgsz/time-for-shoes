04d347e9a51478ab51a62e0cd3a8a32bfc5b70ff
dwddgsz <dwddgsz@gmail.com>
2020-11-12 17:54:08 +0100

    update:clear reducers code

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -14,98 +14,67 @@ const initialState = {
 const cartReducer = (state=initialState,action) => {
     switch(action.type){
         case ADD_TO_CART: {
+            let products;
             if (action.payload.id > 6) {
-                return {...state,watches:state.watches.map(watch=>{
-                    if (watch.id === action.payload.id) {
-                        return {...watch,inCart:true,amount:watch.amount+1};
+                products = 'watches'
+            }
+            else {
+                products = 'shoes'
+            }
+                return {...state,[products]:state[products].map(product=>{
+                    if (product.id === action.payload.id) {
+                        return {...product,inCart:true,amount:product.amount+1};
                     }
                     else {
-                        return watch
+                        return product
                     }
                 }),
                 totalAmount: state.totalAmount + 1,
                 totalValue: state.totalValue + action.payload.value,}
-            } else {
-                return {
-                    ...state,
-                    shoes:state.shoes.map(shoe=>{
-                    if (shoe.id === action.payload.id) {
-                        return {...shoe,inCart:true,amount:shoe.amount+1};
-                    } else {
-                        return shoe
-                    }
-                    }),
-                    totalAmount: state.totalAmount + 1,
-                    totalValue: state.totalValue + action.payload.value,
-                    
-            }
-            }
+
         }
         case INCREASE: {
+            let products;
             if (action.payload.id > 6) {
-                return {...state,watches:state.watches.map(watch=>{
-                    if (watch.id === action.payload.id) {
-                        return {...watch,amount:watch.amount+1};
+                products = 'watches'
+            }
+            else {
+                products = 'shoes'
+            }
+                return {...state,[products]:state[products].map(product=>{
+                    if (product.id === action.payload.id) {
+                        return {...product,amount:product.amount+1};
                     }
                     else {
-                        return watch
+                        return product
                     }
                 }),
                 totalAmount: state.totalAmount + 1,
                 totalValue: state.totalValue + action.payload.value,}
-            } else {
-                return {
-                    ...state,
-                    shoes:state.shoes.map(shoe=>{
-                    if (shoe.id === action.payload.id) {
-                        return {...shoe,amount:shoe.amount+1};
-                    } else {
-                        return shoe
-                    }
-                    }),
-                    totalAmount: state.totalAmount + 1,
-                    totalValue: state.totalValue + action.payload.value,
-                    
-            }
-            }
         }
         case DECREASE: {
+            let products;
             if (action.payload.id > 6) {
-                return {...state,watches:state.watches.map(watch=>{
-                    if (watch.id === action.payload.id) {
-                        if (watch.amount === 1) {
-                            return {...watch, amount: watch.amount - 1,inCart:false}
+                products = 'watches'
+            }
+            else {
+                products = 'shoes'
+            }
+                return {...state,[products]:state[products].map(product=>{
+                    if (product.id === action.payload.id) {
+                        if (product.amount === 1) {
+                            return {...product, amount: product.amount - 1,inCart:false}
                         } else {
-                            return {...watch, amount: watch.amount - 1}
+                            return {...product, amount: product.amount - 1}
                         }
                     }
                     else {
-                        return watch
+                        return product
                     }
                 }),
                 totalAmount: state.totalAmount - 1,
                 totalValue: state.totalValue - action.payload.value,}
-            } else {
-                return {
-                    ...state,
-                    shoes:state.shoes.map(shoe=>{
-                        if (shoe.id === action.payload.id) {
-                            if (shoe.amount === 1) {
-                                return {...shoe, amount: shoe.amount - 1,inCart:false}
-                            } else {
-                                return {...shoe, amount: shoe.amount - 1}
-                            }
-                        }
-                        else {
-                            return shoe
-                        }
-                    }),
-                    totalAmount: state.totalAmount - 1,
-                    totalValue: state.totalValue - action.payload.value,
-                    
-            }
-            }
-        }
+            } 
         default: {
             return state
         }
@@ -116,3 +85,4 @@ const cartReducer = (state=initialState,action) => {
 export default combineReducers({
     cart: cartReducer,
 })
+
